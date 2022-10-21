f198b1f12ce9f3fa53f25e236d0ca83ae6d27e9a
rodolforcampos <rodolforibeirocampos@gmail.com>
2022-10-20 23:01:57 -0300

    IMPLEMENTAR FUNÇÃO QUE CALCULA O PREÇO E MOSTRA NO CONSOLE

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 var nome = "Rodolfo";
 var idade = "27";
+var possuiFaculdade = true;
 
-console.log(nome, idade);
+console.log(nome, idade, possuiFaculdade, nome);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
