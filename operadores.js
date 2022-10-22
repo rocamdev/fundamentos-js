f3fe61efcae52fcdc6c6891f23a70bf30b85ef1d
rocamdev <rodolforibeirocampos@gmail.com>
2022-10-22 18:05:07 -0300

    DEMONSTRAR OPERADORES

---
+++ operadores.js
@@ -0,0 +1,8 @@
+var idade = 23;
+console.log(idade);
+var soma = 100 + 50;
+var subtracao = 100 - 50;
+var multiplicacao = 100 * 50;
+var divisao = 100 / 50;
+var expoente = 100 ** 50;
+var modulo = 100 % 50;
