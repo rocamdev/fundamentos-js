88072199c47539d9cb0cf8d9318954bb3ae36a5b
rocamdev <rodolforibeirocampos@gmail.com>
2022-10-22 18:10:50 -0300

    DEMONSTRAÇÃO DE OPERADORES NUMERICOS E STRINGS

--- operadores.js
+++ operadores.js
@@ -1,8 +1,15 @@
 var idade = 23;
 console.log(idade);
+//OPERADORES NUMERICOS
 var soma = 100 + 50;
 var subtracao = 100 - 50;
 var multiplicacao = 100 * 50;
 var divisao = 100 / 50;
 var expoente = 100 ** 50;
 var modulo = 100 % 50;
+
+//Operadores numéricos e strings
+console.log("100" + 50);
+console.log("100" - 50);
+console.log("100" * "50");
+console.log("comprei 100" / 50);
