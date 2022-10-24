25f59ac72d42084e21176a10eff8c3f49e778540
rocamdev <rodolforibeirocampos@gmail.com>
2022-10-24 16:26:17 -0300

    DEMONSTRAR UMA FUNÇÃO COM STRINGS

--- funcoes.js
+++ funcoes.js
@@ -2,3 +2,14 @@ function areaQadrado(lado) {
   return lado * lado;
 }
 console.log(areaQadrado(4));
+
+// FUNCAO COR FAVORITA
+function corFavorita(cor) {
+  if (cor == "azul") {
+    return "eu gosto do ceu";
+  } else if (cor == "verde") {
+    return "Eu gosto de mato";
+  } else {
+    return "não gosta de ceu nem de mato";
+  }
+}
