57c29065f614e5c86d6d77c305791e6539f98707
rocamdev <rodolforibeirocampos@gmail.com>
2022-10-25 20:05:06 -0300

    DEMONSTRAR DIFERENTES TIPOS DE RETORNO DE UMA FUNÇÃO

--- funcoes.js
+++ funcoes.js
@@ -20,8 +20,24 @@ addEventListener("click", function () {
 });
 
 // VALORES RETORNADOS
+//Função sem return retorna Undefined
 function imc(peso, altura) {
   const imc = peso / altura ** 2;
   console.log(imc);
 }
 console.log(imc(73, 1.76));
+
+//valores retornados
+function terceiraIdade(idade) {
+  console.log(typeof idade);
+  if (typeof idade !== "number") {
+    return "informe sua idade";
+  } else if (idade >= 60) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(terceiraIdade(61));
+console.log(terceiraIdade("oi"));
