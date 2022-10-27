57c8c4ee5e923ebbf42b7e083a6d65dd4dd87d5d
rocamdev <rodolforibeirocampos@gmail.com>
2022-10-27 20:55:18 -0300

    DEMONSTRAR COMO ACESSAR UMA PROPRIEDADE DO OBJETO.

--- objetos.js
+++ objetos.js
@@ -1,6 +1,10 @@
+// Criando um objeto
 var pessoa = {
   nome: "Rodolfo",
   idade: 28,
   profissao: "Frontend Developer",
   temExperiencia: true,
 };
+
+//Acessando propriedades do objeto
+console.log(pessoa.nome);
