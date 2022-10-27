df1ae98415e792ef2bb99c230a24ddcedd85a4ca
rocamdev <rodolforibeirocampos@gmail.com>
2022-10-27 20:59:31 -0300

    DEMONSTRAR CRIAÇÃO DE UM OBJETO COM UMA FUNÇÃO COMO PROPRIEDADE.

--- objetos.js
+++ objetos.js
@@ -8,3 +8,14 @@ var pessoa = {
 
 //Acessando propriedades do objeto
 console.log(pessoa.nome);
+
+// Objeto com uma função como propriedade
+var quadrado = {
+  lados: 4,
+  area: function (lado) {
+    return lado * lado;
+  },
+  perimetro: function (lado) {
+    return this.lados * lado;
+  },
+};
