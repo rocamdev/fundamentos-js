7ee57c73eab796174c6a7a11f44bbb41b143a350
rocamdev <rodolforibeirocampos@gmail.com>
2022-10-28 17:36:56 -0300

    LISTAR OS EXERCÍCIOS DA AULA.

--- objetos.js
+++ objetos.js
@@ -35,3 +35,11 @@ console.log(btn.innerText);
 btn.addEventListener("click", function () {
   console.log("Clicou");
 });
+
+//EXERCICIOS
+//Nomei 3 propriedades ou métodos de strings
+
+//nomeio 5 propriedades ou métodos de elementos do DOM
+
+//busque na web um objeto (método) capaz de interagir com o clipboard,
+//Clipboard é a parte do seu computador que lida com o CTRL+C e CTRL +V
