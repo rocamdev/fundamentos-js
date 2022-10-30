a649093f1798168a535d92f262ca8e608077244b
rocamdev <rodolforibeirocampos@gmail.com>
2022-10-30 20:16:54 -0300

    DEMONSTRAR IMPLEMENTAÇÃO DE UM ARRAY.

--- arrays.js
+++ arrays.js
@@ -3,3 +3,8 @@ var videoGames = ["Switch", "PS4", "XBox"];
 //Métodos
 videoGames.pop(); //Retorna e remove o ultimo item
 videoGames.push("Nintendo"); //Add um item à ultima posição
+
+//Loops
+for (var numero = 0; numero < 10; numero++) {
+  console.log(`Array n ${numero}`);
+}
