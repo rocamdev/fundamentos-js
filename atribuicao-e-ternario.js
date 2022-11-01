ae3c7af22968753a9625e822c49060413ff5eaf9
rocamdev <rodolforibeirocampos@gmail.com>
2022-11-01 17:13:56 -0300

    IMPLEMENTAR OS CÓDIGOS DOS EXERCÍCIOS LISTADOS.

--- atribuicao-e-ternario.js
+++ atribuicao-e-ternario.js
@@ -7,3 +7,16 @@ numero %= 10;
 //operador ternário
 idade = 19;
 var podeBeber = idade >= 18 ? "Pode beber." : "Não pode beber.";
+
+//Exercícios
+//some 500 ao valor de scroll abaixo, atribuindo o novo valor ao scroll
+var scroll = 1000;
+scroll += 500;
+
+//atribua true para a variavel darCredito, caso o cliente possua carro e casa. E false caso o contrário.
+
+var possuiCarro = true;
+var possuiCasa = true;
+var darCredito;
+
+darCredito = possuiCarro == true && possuiCasa == true;
