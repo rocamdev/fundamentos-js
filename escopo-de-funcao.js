d3a2d41366be150837abcf3d37f78a26b7d3910c
rocamdev <rodolforibeirocampos@gmail.com>
2022-11-04 21:00:36 -0300

    DEMONSTRAR VARIÁVEL VAZANDODO ESCOPO. Isso ocorre porque quando crianmos um var em um bloco ele é acessivel fora do bloco.

--- escopo-de-funcao.js
+++ escopo-de-funcao.js
@@ -2,3 +2,10 @@ function mostrarCarro() {
   var carro = "Fusca"; //Erro criando variável global
   console.log(carro);
 }
+
+//VARIÁVEL VAZANDO DO ESCOPO
+if (true) {
+  var mes = "Dezembro";
+  console.log(mes);
+}
+console.log(mes);
